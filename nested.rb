e0bcfc1eb12e396f058b43d53cd358d8bf122d29
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-08 20:53:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     #programmer_hash.each do |key, value|
+      programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+      return programmer_hash
 end
